81c96482c44dbf870db672e8983c6819b0f8c064
atalibaneto <atalibaneto@gmail.com>
2025-02-25 15:36:51 -0300

    Aula 25/02/2025 - [INTERVALO]

---
+++ DesenvolveWEB/php/aula6/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 6</title>
+</head>
+<body>
+    <?php
+        $valor = $_GET['valor'] ?? NULL;
+    ?>
+    <h2>Digite um número para a tabuada</h2>
+    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="get">
+     <input type="number" name="valor" value="<?php $valor ?>">
+     <?php 
+             if ($valor != 0) {
+     echo "<h2>Resultado da tabuada</h2>";
+     }
+     ?>
+     <?php
+        if ($valor != 0) {
+            for ($i = 0; $i <= 10; $i++)
+            {
+                echo "$valor x $i = ".($valor * $i)."<br>";
+            }
+    };
+     ?>
+</body>
+</html>
---
+++ DesenvolveWEB/php/aula6/tab.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <?php 
+        $num = $_POST["numero"];
+        for ($i = 0; $i <= 10; $i++)
+        {
+            echo "$num x $i = ".$num * $i."<br>";
+        }
+
+    ?>
+</body>
+</html>
