bd69c5b88364f5a095259398de8a50e97c688cdd
atalibaneto <atalibaneto@users.noreply.github.com>
2025-03-12 12:07:22 -0300

    Update frmCadastroPaciente.php 12/03/2025

--- DesenvolveWEB/enfermagem/frmCadastroPaciente.php
+++ DesenvolveWEB/enfermagem/frmCadastroPaciente.php
@@ -3,10 +3,19 @@ session_start();
 require "banco/conexao.php";
 
 if (isset($_POST['cadastroPaciente'])) {
-    $nome = mysqli_real_escape_string($conexao, trim($_POST['nomePaciente']));
-    $dataNasc = mysqli_real_escape_string($conexao, trim($_POST['dtNascPaciente']));
+    $nomePaciente = mysqli_real_escape_string($conexao, trim($_POST['nomePaciente']));
+    $dtNascPaciente = mysqli_real_escape_string($conexao, trim($_POST['dtNascPaciente']));
     $sexo = mysqli_real_escape_string($conexao, trim($_POST['sexo']));
     $nomeMae = mysqli_real_escape_string($conexao, trim($_POST['nomeMae']));
     $endereco = mysqli_real_escape_string($conexao, trim($_POST['endereco']));
+    $dtInternacao = mysqli_real_escape_string($conexao, trim($_POST['dtInternacao']));
+
+    $sql = "INSERT INTO pacientes (nomePaciente, dtNascPaciente, sexoPaciente, endPaciente, nomeMaePaciente, dtInternacaoPaciente) VALUES ('$nomePaciente', '$dtNascPaciente', '$sexo', '$nomeMae', '$endereco', '$dtInternacao')";
+
+    if (mysqli_query($conexao, $sql)) {
+        echo "Paciente cadastrado com sucesso!";
+    } else {
+        echo "Erro ao cadastrar paciente: " . mysqli_error($conexao);
+    }
 }
 ?>
